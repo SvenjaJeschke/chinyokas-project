00e6215ff9766fc450cc39a0a60c64248085d9b2
SvenjaJeschke <svenja.jeschke12@gmail.com>
2020-03-26 08:18:00 +0100

    trying to put the backend in an own project

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 3000; // backend 3000, frontend 5000!
+var port = process.env.PORT || 5000; // backend 3000, frontend 5000!
 app.listen(port);
 console.log('server started ' + port);
