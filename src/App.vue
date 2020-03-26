ba7219c731187c729b480ae623444a80bb4d2f13
SvenjaJeschke <svenja.jeschke12@gmail.com>
2020-03-26 09:02:36 +0100

    meh

--- src/App.vue
+++ src/App.vue
@@ -99,7 +99,7 @@ export default {
             const password = localStorage.getItem('password');
             this.axios
                 .get(
-                  '/api/auth/checkadmin.php',
+                  '/backend/api/auth/checkadmin.php',
                   { params: { password: password } },
                   { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } })
                 .then(response => {
@@ -108,7 +108,7 @@ export default {
         },
         getTheme() {
             this.axios
-                .get('/api/theme/current.php')
+                .get('/backend/api/theme/current.php')
                 .then(response => {
                     this.theme = response.data;
                 })
--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import VueAxios from 'vue-axios';
 import VueDayjs from 'vue-dayjs-plugin';
 import TextareaAutosize from 'vue-textarea-autosize';
 
-axios.defaults.baseURL = 'https://chinyokas-project-backend.herokuapp.com/';
+axios.defaults.baseURL = 'http://localhost:8080';
  
 Vue.use(TextareaAutosize);
 Vue.use(VueAxios, axios);
